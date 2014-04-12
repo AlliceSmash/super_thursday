0470fc17b060cf282064ec39013d42876ac43a64
AlliceSmash <rustic_baker@outlook.com>
2014-04-12 17:40:24 -0400

    add thumbnails

--- script/slideshow.js
+++ script/slideshow.js
@@ -1,37 +1,65 @@
-// JavaScript source code
 $(window).load(function () {
     var timer;
-    function showNextSlide() {
+    function showSlide(direction, fadeSpeed) {
         var $active = $("#slides img.active");
-        var $temp = $active.next('img');
-        var $next = $temp.length ? $temp : $('#slides img:first-child');
+        var $activeNav = $("#tbncontainer .activeNav");
+        var temp, $next, $nextNav;
+        if( typeof(speed) === "undefined" ) { speed =1000;};
 
-        $active.fadeOut({ duration: 1000 }).removeClass("active");
-        $next.fadeIn({ duration: 1000 }).addClass("active");
+        $next = getNextElement($active, direction);
+        $nextNav = getNextElement($activeNav, direction);
+        $active.fadeOut({ duration: fadeSpeed }).removeClass("active");
+        $activeNav.removeClass("activeNav");
+        $nextNav.addClass("activeNav");
+        $next.fadeIn({ duration: fadeSpeed }).addClass("active");
+    }
+
+    function getNextElement(currElement, direction) {
+        if (direction.toLowerCase() === "next") {
+            $temp = currElement.next('img');
+            nextElement = $temp.length ? $temp : currElement.parent().children(':first-child');
+        } else {
+            $temp = currElement.prev('img');
+            nextElement = $temp.length ? $temp : currElement.parent().children(':last-child');
+        }
+        return nextElement;
+    }
+
+    function showNextSlide() {
+        showSlide("next", 1000);
     };
 
     function showPrevSlide() {
-        var $active = $("#slides img.active");
-        var $temp = $active.prev('img');
-        var $next = $temp.length ? $temp : $('#slides img:last-child');
-        $active.fadeOut({ duration: 1000 }).removeClass("active");
-        $next.fadeIn({ duration: 1000 }).addClass("active");
+        showSlide("prev", 1000);
     };
 
-    $("#controls #right").click(function () {
+    $("#right").click(function () {
         clearInterval(timer);
         showNextSlide();
         timer = setInterval(showNextSlide, 5000);
     });
 
-    $("#controls #left").click(function () {
+    $("#left").click(function () {
         clearInterval(timer);
         showPrevSlide();
         timer = setInterval(showNextSlide, 5000);
     });
 
+    $("#scrollleft").click(function () {
+        showSlide("prev", 1000);
+    });
+
+    $("#scrollright").click(function () {
+        showSlide("next", 1000);
+    });
+
+    $("#tbncontainer img").hover(function () {
+        //var index = $(this).index();
+        //var $active = $("#container img:eq(index)");
+    });
+
     (function () {
         $("#slides img:not(.active)").hide();
-        timer = setInterval( showNextSlide, 5000 );
+        //timer = setInterval( showNextSlide, 5000 );
     })();
 });
