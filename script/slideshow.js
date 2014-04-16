15ef85eb122f3569674f333ef0057d7df0f4abd8
AlliceSmash <rustic_baker@outlook.com>
2014-04-15 21:19:13 -0400

    add thumbnails and sliding function

--- script/slideshow.js
+++ script/slideshow.js
@@ -11,6 +11,7 @@ $(window).load(function () {
         $next = getNextElement($active, direction);
         $nextNav = getNextElement($activeNav, direction);
         doTransition($active, $next, $activeNav, $nextNav, fadespeed);
+        //changeOridinal(direction);
     }
 
     function getNextElement(currElement, direction) {
@@ -31,13 +32,15 @@ $(window).load(function () {
         nextElement.fadeIn({ duration: fadespeed }).addClass("active");
     }
 
-    function showNextSlide() {
-        showSlide("next");
-    };
-
-    function showPrevSlide() {
-        showSlide("prev");
-    };
+    $("#tbncontainer img").click(function () {
+        //find current slide to fade
+        var $currElem = $("#container img.active");
+        var $currNav = $("#tbncontainer img.activeNav");
+        //find which one to show
+        var index = $(this).index();
+        var $toShow = $("#container img").eq(index);
+        doTransition($currElem, $toShow, $currNav, $(this), 600);
+    });
 
     $("#right").click(function () {
 //        clearInterval(timer);
@@ -52,25 +55,69 @@ $(window).load(function () {
     });
 
     $("#scrollleft").click(function () {
-        showSlide("prev");
+        //showSlide("prev");
+        changeOridinal("prev");
     });
 
     $("#scrollright").click(function () {
-        showSlide("next");
+        //showSlide("next");
+        changeOridinal("next");
     });
 
-    $("#tbncontainer img").click(function () {
-        //find current slide to fade
-        var $currElem = $("#container img.active");
-        var $currNav = $("#tbncontainer img.activeNav");
-        //find which one to show
-        var index = $(this).index();
-        var $toShow = $("#container img").eq(index);
-        doTransition($currElem, $toShow, $currNav, $(this), 600);
+    function showNextSlide() {
+        showSlide("next");
+    };
+
+    function showPrevSlide() {
+        showSlide("prev");
+    };
+
+    function changeOridinal(direction) {
+        var length = $("#tbncontainer").children().length;
+        var displayOrder = 0;
+        var orderGroup = Modernizr.prefixed('order');
+
+        for (var i = 0; i < length; i++) {
+            var child = $("#tbncontainer").children().eq(i)[0];
+            if (child && child.style) {
+                if (direction === "next") {
+                    displayOrder = parseInt(child.style[orderGroup]) - 1;
+                    child.style[orderGroup] = displayOrder <= 0 ? length : displayOrder;
+                } else {
+                    displayOrder = parseInt(child.style[orderGroup]) + 1;
+                    child.style[orderGroup] = displayOrder > length ? displayOrder % length : displayOrder;
+                }
+            }
+        }
+    };
+
+    function setInitDisplayOrder() {
+        var length = $("#tbncontainer").children().length;
+        var displayOrder = 0;
+        var navIndex = 0;
+        var orderGroup = Modernizr.prefixed('order');
+
+        while (displayOrder < length) {
+            var child = $("#tbncontainer").children().eq(navIndex)[0];
+            if (child && child.style) {
+                displayOrder++;
+                child.style[orderGroup] = displayOrder;
+            }
+            navIndex++;
+        };
+    }
+
+    $("#container").hover(function () {
+        $(this).css('cursor', 'pointer');
+        clearInterval(timer);
+        },
+        function () {
+            $(this).css('cursor', 'auto');
+            timer = setInterval(showNextSlide, 5000);
     });
 
     (function () {
-        
-       // timer = setInterval( showNextSlide, 5000 );
+       setInitDisplayOrder();
+       timer = setInterval( showNextSlide, 5000 );
     })();
 });
