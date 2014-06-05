4a9bb8e1a12a60c494c1b41e549ec80a8a4b31ed
AlliceSmash <rustic_baker@outlook.com>
2014-06-04 21:14:14 -0400

    first kata: fizzbuzz

---
+++ Kata/FizzBuzz/FizzBuzz/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NumberGame
+{
+    public class Game1
+    {
+        public string FizzBuzz(int input)
+        {
+            if (input % 15 == 0) return "FizzBuzz";
+            if (input % 3 == 0) return "Fizz";
+            if (input % 5 == 0) return "Buzz";
+            string output = input.ToString();
+            return output;
+        }
+    }
+}
---
+++ Kata/FizzBuzz/FizzBuzzTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberGame;
+
+namespace NumberGameTest
+{
+    [TestClass]
+    public class Game1Test_Should_
+    {
+        private TestContext testContextInstance;
+        public Game1 nameGame;
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        [TestInitialize]
+        public void Init()
+        {
+            nameGame = new Game1();
+        }
+
+        [TestMethod]
+        [DeploymentItem("NumberGameTest\\Expected.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                           "|DataDirectory|\\Expected.xml",
+                           "Row",
+                     DataAccessMethod.Sequential)]
+        public void FizzBuzz()
+        {
+            int input = Int32.Parse((string)testContextInstance.DataRow["Input"]);
+            string expected = (string)testContextInstance.DataRow["Result"];
+            string actual = nameGame.FizzBuzz(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
